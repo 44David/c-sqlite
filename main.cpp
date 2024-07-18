2081fb8f14295cb2a3fc8070564ac31177c873df
David Soane <david.soane@outlook.com>
2024-07-17 19:39:52 -0600

    added file reading for sql query

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #include <sqlite3.h>
-
-using namespace std;
+#include <fstream>
 
 int main(int argc, char** argv) {
     sqlite* DB;
+
+    std::ifstream ifs("sql-query.txt");
+    std::string content( ( std::istreambuf_iterator<char>(ifs)),
+                        (std::istreambuf_iterator<char>() ) );
 
     int exit = 0;
     exit = sqlite3_open("practice-c++.db", &DB);
 
     if (exit) {
-        cerr << "Error opening DB" << sqlite3_errmsg(DB) << endl;
+        std::cerr << "Error opening DB" << sqlite3_errmsg(DB) <<  std::endl;
         return -1;
 
     } else {
-        cout << "Database was opened" << endl;
+         std::cout << "Database was opened" <<  std::endl;
         sqlite3_close(DB);
         return 0
     }
